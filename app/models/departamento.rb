3d651084f487ced5e3b5ba14b65504eea88a3073
ivanwilfredo <ivanwilfredo@gmail.com>
2016-05-23 04:41:48 +0000

    modificandopantallabienvenida

--- app/models/departamento.rb
+++ app/models/departamento.rb
@@ -1,2 +1,7 @@
 class Departamento < ActiveRecord::Base
+    has_many :distritos
+    validates :codigo_departamento, presence: true, uniqueness: {case_sensitive: false}
+    def name
+     self.nombre_departamento
+	end
 end
--- app/models/distrito.rb
+++ app/models/distrito.rb
@@ -1,3 +1,9 @@
 class Distrito < ActiveRecord::Base
   belongs_to :departamento
+   validates :cod_distrito, presence: true
+ 
+  #Código Ivargas 22/05/2016 enlazando clave foranea departamento
+  def departamento_name
+  	self.departamento.nombre_departamento
+  end
 end
